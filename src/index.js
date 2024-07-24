7bfa63886b4563850a0f45a38c1d9bb2b8a228de
Faisal Imtiyaz <faisalimtiyazlone@gmai.com>
2024-07-24 11:48:02 +0530

    util code discussion

--- src/index.js
+++ src/index.js
@@ -12,23 +12,34 @@ dotenv.config({
 
 
 connectDB()
-/*
-1st approach------------->>>
-const app = express()
-import express from "express"
-(async()=>{
-    try{
-       await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-       app.on("error",(error)=>{
-        console.log("Error:",erroe);
-        throw error
-       })
-       app.listen(process.env.PORT,()=>{
-        console.log(`App is listening on port ${process.abort.env.PORT}`);
-       })
-    }catch(error){
-        console.log("ERROR:",error)
-        throw err
-    }
-})()
-    */
+
+.then((()=>{
+    app.liste(process.env.PORT || 8000,()=>{
+        console.log( `SEEerver is running at port: ${process.env.PORT}`);
+    })
+})
+.catch( (err)=>{
+    console.log("Mongodb connection failed !!!!", err);
+
+})
+
+)   
+
+//1st approach------------->>>
+// const app = express()
+// import express from "express"
+// (async()=>{
+//     try{
+//        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+//        app.on("error",(error)=>{
+//         console.log("Error:",erroe);
+//         throw error
+//        })
+//        app.listen(process.env.PORT,()=>{
+//         console.log(`App is listening on port ${process.abort.env.PORT}`);
+//        })
+//     }catch(error){
+//         console.log("ERROR:",error)
+//         throw err
+//     }
+// }
