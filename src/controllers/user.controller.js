010c901bde4a8b6366ab31c8a9ffd994a86d4e72
Faisal Imtiyaz <faisalimtiyazlone@gmai.com>
2024-08-02 12:15:31 +0530

    change

---
+++ src/controllers/user.controller.js
@@ -0,0 +1,16 @@
+import { hashSync } from "bcrypt";
+import { asyncHandler } from "../utils/asyncHandler";
+
+
+const registerUser=asyncHandler(async (req,res)=>{
+    return res.status(200).json({
+        message:"ok"
+    })
+})
+
+
+
+export  {
+    registerUser
+
+}
